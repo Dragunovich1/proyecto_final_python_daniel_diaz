a95cef97187fb909fae89ffafe82b0f41208b6b0
Daniel diaz <danielciarlantini@gmail.com>
2023-10-10 15:27:26 -0300

    minor changes

--- blogfierros/members/urls.py
+++ blogfierros/members/urls.py
@@ -15,9 +15,10 @@ urlpatterns = [
 	path('profile_edit_success', views.profile_edit_success, name="profile_edit_success"),
 	path('<int:pk>/profile/', ShowProfilePageView.as_view(), name='show_profile_page'),
 	path('<int:pk>/edit_profile_page/', EditProfilePageView.as_view(), name='edit_profile_page'),
-	path('create_profile_page/', CreateProfilePageView.as_view(), name='create_profile_page'),
+	path('create_user_profile_page/', CreateProfilePageView.as_view(), name='create_user_profile_page'),
 	path('profile_page_edit_success', views.profile_page_edit_success, name="profile_page_edit_success"),
 	path('profile_list', views.profile_list, name="profile_list"),
 	path('profile_page_edit_success', views.profile_page_edit_success, name="profile_page_edit_success"),
 	path('profile_page_crete_success', views.profile_page_create_success, name="profile_page_create_success"),
+	path('create_profile_page/', CreateProfilePageView.as_view(), name='create_profile_page'),
 ]
